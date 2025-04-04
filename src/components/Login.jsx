8f00211e7a45ef7b408b0603f005e0efe5c56d19
karthikathota <saikarthik.tech@gmail.com>
2025-04-04 09:46:46 +0530

    axios added and cors error rectified

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,27 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("");
-  const [password, setPassword] = useState("");
+  const [emailId, setEmailId] = useState("dhoni@gmail.com");
+  const [password, setPassword] = useState("madhavi");
 
   //Handling the user login credentials
-  const handleLogin = async () => {};
+  const handleLogin = async () => {
+    try {
+      console.log("Sending login request...");
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log("Response:", res); // Log the response
+    } catch (error) {
+      console.log("Error:", error.message); // Log any error
+    }
+  };
 
   return (
     <div className="flex justify-center my-40">
@@ -37,7 +53,9 @@ const Login = () => {
             </fieldset>
           </div>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
