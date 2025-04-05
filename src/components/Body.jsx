c8cc2cf7f41a3c35993099faf1a1ef527904cf01
karthikathota <saikarthik.tech@gmail.com>
2025-04-05 10:59:34 +0530

    User login functionality improved when page is refreshed with the help of token

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux"; // Missing this import
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(user.data));
+      console.log(user.data);
+    } catch (error) {
+      if (error.status === 401) {
+        navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <div>
       <Navbar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   const user = useSelector((store) => store.user);
 
-  console.log(user);
   return (
     <div className="navbar bg-base-200 shadow-sm">
       <div className="flex-1">
