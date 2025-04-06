fca09807c584f968aeef0d03138af3e4aea63c8a
karthikathota <saikarthik.tech@gmail.com>
2025-04-06 12:50:46 +0530

    Navbar functionaltiy improved

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,12 +5,17 @@ import Footer from "./Footer";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { addUser } from "../utils/userSlice";
-import { useDispatch } from "react-redux"; // Missing this import
+import { useDispatch, useSelector } from "react-redux"; // Missing this import
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  // Checking is userData is present in redux-store
+  const userData = useSelector((store) => store.user);
+
   const fetchUser = async () => {
+    if (userData) return;
     try {
       const user = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
